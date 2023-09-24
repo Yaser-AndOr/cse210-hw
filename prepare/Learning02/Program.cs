24fb6f80b28d6290cc669df043482a3b2acf13b1
Nefitoo4 <nefizaldana@gmail.com>
2023-09-23 22:58:18 -0600

    learning02 developed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobtitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobtitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Nefi Zaldana";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
+    public class Job
+    {
+        public string _jobtitle;
+        public string _company;
+        public int _startYear;
+        public int _endYear;
+    }
+
+    public class Resume
+    {
+        public string _name;
+        List<Job> _jobs = new List<Job>();
+    }
+
 }
